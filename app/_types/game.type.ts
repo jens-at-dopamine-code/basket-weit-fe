14ca92ce32ee76ba158d42859f71989bbb0bfbe4
jens@dopamin-code <jens@dopamine-code.com>
2023-11-26 21:50:52 +0100

    update Game type

--- app/_types/game.type.ts
+++ app/_types/game.type.ts
@@ -1,13 +1,14 @@
 import type { Team } from './team.type';
 
 export type Game = {
-  id: number;
   awayTeamId: Team['id'];
   awayTeamName: Team['name'];
-  awayTeamScore?: number;
   date: string;
   gameType: 'competition' | 'cup';
   homeTeamId: Team['id'];
   homeTeamName: Team['name'];
+  id: number;
+  series: string;
+  awayTeamScore?: number;
   homeTeamScore?: number;
 };
